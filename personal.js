10c3ec9311b98e3eeceb27ed78ab656c0fb9532c
Sinem <sinem93@gmail.com>
2016-05-31 23:39:34 +0300

    navigation to portfolio page added
     + some styling changes

--- personal.js
+++ personal.js
@@ -346,7 +346,7 @@ angular.module("personalWeb").controller("homeController",
                 "and an art lover. I graduated from Oberlin College with Computer Science and Mathematics " +
                 "major, then moved to Istanbul and started working at Monitise MEA (Pozitron as it used " +
                 "to be called). More information about my educational background can be found in my resume." +
-                "As for work, I have been mostly working on <a href=\"https://mobile.flypgs.com/#/checkin/search\">" +
+                " As for work, I have been mostly working on <a href=\"https://mobile.flypgs.com/#/checkin/search\">" +
                 "Pegasus Airlines' mobile website </a>";
 
             var links = "Here are links to: </br>" +
@@ -373,5 +373,9 @@ angular.module("personalWeb").controller("homeController",
 
             this.openCv = function () {
                 window.open("assets/file/cv.pdf");
-            }
+            };
+
+            this.navigateToPortfolio = function () {
+                window.location = ("portfolio.html");
+            };
         }]);
